51c2f02825c7908b295afe61382c89985e4ade34
Charles Hoot <hoot@nwmissouri.edu>
2021-03-18 02:12:57 -0500

    new endpoint

--- routes/name.js
+++ routes/name.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET my data page. */
 router.get('/', function(req, res, next) {
-  res.render('name', { title: 'Charles Hoot' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
